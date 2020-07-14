52cb47516b86499117d0abb5bcdc423b0397754a
JosephSteelePerkins <45665579+JosephSteelePerkins@users.noreply.github.com>
2020-07-14 14:46:16 +0100

    stuff

---
+++ My Stuff/MySQLStuff/SSISDB execute package.sql
@@ -0,0 +1,27 @@
+
+use SSISDB
+
+select * from [SSISDB].[internal].[packages]
+
+select * from ssisdb.catalog.environment_references
+
+
+
+
+Declare @execution_id bigint
+
+EXEC [SSISDB].[catalog].[create_execution] @package_name=N'Load into ETL_Staging.dtsx',
+    @execution_id=@execution_id OUTPUT,
+    @folder_name=N'Diamond',
+	  @project_name=N'Diamond',
+  	@use32bitruntime=False,
+	  @reference_id=1
+Select @execution_id
+DECLARE @var0 smallint = 1
+EXEC [SSISDB].[catalog].[set_execution_parameter_value] @execution_id,
+    @object_type=50,
+	  @parameter_name=N'LOGGING_LEVEL',
+	  @parameter_value=@var0
+EXEC [SSISDB].[catalog].[start_execution] @execution_id
+
+print @execution_id
